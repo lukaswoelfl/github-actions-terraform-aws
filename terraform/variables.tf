e77fd93ff849400f267566bfef86127e31fa1233
Lukas Wölfl <rope-room2r@icloud.com>
2026-06-05 12:23:50 +0200

    fixed region configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,9 @@
 variable "bucket_name" {
-    description = "The name of the S3 bucket to create"
-    type        = string
-    
-  
+  description = "The name of the S3 bucket to create"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region used by the AWS provider"
+  type        = string
 }
